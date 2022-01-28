df2df00f9bec7f1eba7c80cfec05b0fc1588e63e
AshIgnYeo <contact@ashleyignatius.com>
2022-01-28 12:42:07 +0800

    updated about to use page component

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,16 @@
 import React from "react";
+import Page from "../components/page";
 
 const About = (props) => {
   return (
-    <div>
+    <Page>
       <h1>This is the about page</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores accusamus earum necessitatibus optio amet
         dolores maiores esse quos qui placeat, mollitia aliquid obcaecati eaque rem accusantium maxime veritatis
         repudiandae iure.
       </p>
-    </div>
+    </Page>
   );
 };
 
